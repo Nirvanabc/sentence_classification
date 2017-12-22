91e78e9c777c994cac331763445b20cd2d140890
Nirvana <muinova@yandex.ru>
2017-12-22 19:29:19 +0300

    new restore file

--- restored_tensor.py
+++ restored_tensor.py
@@ -5,7 +5,7 @@ sent_size = 16
 class_num = 2
 vec_size = 100
 
-saver = tf.train.import_meta_graph('saved/my_model-720.meta')
+saver = tf.train.import_meta_graph('saved/my_model-2080.meta')
 sess = tf.Session()
 saver.restore(sess, tf.train.latest_checkpoint("./saved/"))
 
@@ -15,12 +15,24 @@ x = graph.get_tensor_by_name("x:0")
 y_ = graph.get_tensor_by_name("y_:0")
 keep_prob = graph.get_tensor_by_name("keep_prob:0")
 accuracy = graph.get_tensor_by_name("accuracy:0")
-# train_step = graph.get_tensor_by_name("train_step:0")
-
 corpora_file = 'corpora_MR'
 corpora = open(corpora_file, 'r')
-batch = next_batch(corpora, 50, vec_size)
-sess.run(tf.global_variables_initializer())
-feed_dict ={x:batch[0], y_:batch[1], keep_prob:1.0}
-with sess.as_default():
-    print (sess.run(accuracy, feed_dict))
+
+
+def batch_classifier_online():
+    batch = next_batch(corpora, 50, vec_size)
+    # the next line is mistakable! It sets all variables
+    # to random values!
+    # sess.run(tf.global_variables_initializer())
+    feed_dict ={x:batch[0], y_:batch[1], keep_prob:1.0}
+    with sess.as_default():
+        print (sess.run(accuracy, feed_dict))
+
+
+def sent_classifier_online(sent, label):
+    sent = [sent.split()]
+    sent = prepare_corpora(sent, vec_size, sent_size)
+    feed_dict = {x:sent, y_: [label], keep_prob:1.0}
+    with sess.as_default():
+        print (sess.run(accuracy, feed_dict))
+    
