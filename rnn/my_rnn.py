9c0bd75857f7785ba2d7142af9a6546c13b3e41c
Nirvana <muinova@yandex.ru>
2018-01-27 15:15:12 +0300

    my_rnn (not mine indeed)

--- rnn/my_rnn.py
+++ rnn/my_rnn.py
@@ -2,7 +2,6 @@ import tensorflow as tf
 import numpy as np
 from constants import *
 
-
 def weight_variable(shape):
     initial = tf.truncated_normal(shape, stddev=0.1)
     return tf.Variable(initial)
@@ -15,19 +14,63 @@ hidden_size = 100 # size of hidden layer of neurons
 seq_length = 25 # number of steps to unroll the RNN for
 learning_rate = 1e-1
 
-data = open('input.txt', 'r').read()
-chars = list(set(data))
-data_size, vocab_size = len(data), len(chars)
-print ('data has %d characters, %d unique.' % (
-    data_size, vocab_size))
 
-inputs = tf.placeholder(tf.int32, [batch_size, seq_len])
-targets = tf.placeholder(tf.int32, [batch_size, seq_len])
-keep_prob = tf.placeholder(tf.float32)
+with open('short_input.txt', 'r') as f:
+    text=f.read()
+    vocab = sorted(set(text))
+    vocab_to_int = {c: i for i, c in enumerate(vocab)}
+    int_to_vocab = dict(enumerate(vocab))
+    encoded = np.array([vocab_to_int[c] for c in text], dtype=np.int32)
+    
 
-char_to_ix = { ch:i for i,ch in enumerate(chars) }
-ix_to_char = { i:ch for i,ch in enumerate(chars) }
+def get_batches(arr, n_seqs, n_steps):
+    '''Создаем генератор, который возвращает пакеты размером
+    n_seqs x n_steps из массива arr.
+    
+    Аргументы
+    ---------
+    arr: Массив, из которого получаем пакеты
+    n_seqs: Batch size, количество последовательностей в пакете
+    n_steps: Sequence length, сколько "шагов" делаем в пакете
+    '''
+    # Считаем количество символов на пакет и количество пакетов, которое можем сформировать
+    characters_per_batch = n_seqs * n_steps
+    n_batches = len(arr)//characters_per_batch
+    
+    # Сохраняем в массиве только символы, которые позволяют сформировать целое число пакетов
+    arr = arr[:n_batches * characters_per_batch]
+    
+    # Делаем reshape 1D -> 2D, используя n_seqs как число строк, как на картинке
+    arr = arr.reshape((n_seqs, -1))
+    
+    for n in range(0, arr.shape[1], n_steps):
+        # пакет данных, который будет подаваться на вход сети
+        x = arr[:, n:n+n_steps]
+        # целевой пакет, с которым будем сравнивать предсказание, получаем сдвиганием "x" на один символ вперед
+        y = np.zeros_like(x)
+        y[:, :-1], y[:, -1] = x[:, 1:], x[:, 0]
+        yield x, y
 
+        
+def build_inputs(batch_size, num_steps):
+    ''' Определяем placeholder'ы для входных, целевых данных, а также вероятности drop out
+    
+    Аргументы
+    ---------
+    batch_size: Batch size, количество последовательностей в пакете
+    num_steps: Sequence length, сколько "шагов" делаем в пакете
+    
+    '''
+    # Объявляем placeholder'ы
+    inputs = tf.placeholder(tf.int32, [batch_size, num_steps], name='inputs')
+    targets = tf.placeholder(tf.int32, [batch_size, num_steps], name='targets')
+    
+    # Placeholder для вероятности drop out
+    keep_prob = tf.placeholder(tf.float32, name='keep_prob')
+    
+    return inputs, targets, keep_prob
+
+        
 def build_lstm(lstm_size, num_layers, batch_size, keep_prob):
     def build_cell(lstm_size, keep_prob):
         lstm = tf.contrib.rnn.BasicLSTMCell(lstm_size)
@@ -99,44 +142,41 @@ def build_optimizer(loss, learning_rate, grad_clip):
     optimizer = train_op.apply_gradients(zip(grads, tvars))
     return optimizer
 
-
-def charRNN(num_classes, batch_size=64, num_steps=50,
-             lstm_size=128, num_layers=2, learning_rate=0.001,
-             grad_clip=5, sampling=False):
-    # Мы будем использовать эту же сеть для генерации текста,
-    # при этом будем подавать по одному символу за один раз
-    if sampling == True:
-        batch_size, num_steps = 1, 1
-    else:
-        batch_size, num_steps = batch_size, num_steps
-        tf.reset_default_graph()
+class CharRNN:
+    def __init__(self, num_classes, batch_size=64, num_steps=50,
+                 lstm_size=128, num_layers=2, learning_rate=0.001,
+                 grad_clip=5, sampling=False):
         
-        # Получаем input placeholder'ы
-        inputs, targets, keep_prob = build_inputs(
-            batch_size, num_steps)
-        
-        # Строим LSTM ячейку
-        cell, initial_state = build_lstm(
-            lstm_size, num_layers, batch_size, keep_prob)
-        
-        ## Прогоняем данные через RNN слои
-        # Делаем one-hot кодирование входящих данных
-        x_one_hot = tf.one_hot(inputs, num_classes)
-        
-        # Прогоняем данные через RNN и собираем результаты
-        outputs, state = tf.nn.dynamic_rnn(
-            cell, x_one_hot, initial_state = initial_state)
-        final_state = state
-        
-        # Получаем предсказания (softmax) и рез-т logit-функции
-        prediction, logits = build_output(
-            outputs, lstm_size, num_classes)
-        
-        # Считаем потери и оптимизируем (с обрезкой градиента)
-        loss = build_loss(
-            logits, targets, lstm_size, num_classes)
-        optimizer = build_optimizer(
-            loss, learning_rate, grad_clip)
+        # Мы будем использовать эту же сеть для сэмплирования (генерации текста),
+        # при этом будем подавать по одному символу за один раз
+        if sampling == True:
+            batch_size, num_steps = 1, 1
+        else:
+            batch_size, num_steps = batch_size, num_steps
+            
+            tf.reset_default_graph()
+            
+            # Получаем input placeholder'ы
+            self.inputs, self.targets, self.keep_prob = build_inputs(batch_size, num_steps)
+            
+            # Строим LSTM ячейку
+            cell, self.initial_state = build_lstm(lstm_size, num_layers, batch_size, self.keep_prob)
+            
+            ### Прогоняем данные через RNN слои
+            # Делаем one-hot кодирование входящих данных
+            x_one_hot = tf.one_hot(self.inputs, num_classes)
+            
+            # Прогоняем данные через RNN и собираем результаты
+            outputs, state = tf.nn.dynamic_rnn(cell, x_one_hot, initial_state=self.initial_state)
+            self.final_state = state
+            
+            # Получаем предсказания (softmax) и результат logit-функции
+            self.prediction, self.logits = build_output(outputs, lstm_size, num_classes)
+            
+            # Считаем потери и оптимизируем (с обрезкой градиента)
+            self.loss = build_loss(self.logits, self.targets, lstm_size, num_classes)
+            self.optimizer = build_optimizer(self.loss, learning_rate, grad_clip)
+            
             
 model = CharRNN(len(vocab), batch_size=batch_size,
                 num_steps=num_steps,
@@ -146,40 +186,40 @@ model = CharRNN(len(vocab), batch_size=batch_size,
 saver = tf.train.Saver(max_to_keep=100)
         
 
-with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-    
-    # Можно раскомментировать строчку ниже и продолжить
-    # обучение с checkpoint'а
-    # saver.restore(sess, 'checkpoints/______.ckpt')
-    counter = 0
-    for e in range(epochs):
-        # Обучаем сеть
-        new_state = sess.run(model.initial_state)
-        loss = 0
-        for x, y in get_batches(encoded, batch_size, num_steps):
-            counter += 1
-            start = time.time()
-            feed = {model.inputs: x,
-                    model.targets: y,
-                    model.keep_prob: keep_prob,
-                    model.initial_state: new_state}
-            batch_loss, new_state, _ = sess.run(
-                [model.loss,
-                 model.final_state,
-                 model.optimizer],
-                feed_dict=feed)
-            
-            end = time.time()
-            print('Epoch: {}/{}... '.format(e+1, epochs),
-                  'Training Step: {}... '.format(counter),
-                  'Training loss: {:.4f}... '.format(batch_loss),
-                  '{:.4f} sec/batch'.format((end-start)))
-            
-            if (counter % save_every_n == 0):
-                saver.save(sess, "checkpoints/i{}_l{}.ckpt".format(counter, lstm_size))
-                
-    saver.save(sess, "checkpoints/i{}_l{}.ckpt".format(counter, lstm_size))
+# with tf.Session() as sess:
+#     sess.run(tf.global_variables_initializer())
+#     
+#     # Можно раскомментировать строчку ниже и продолжить
+#     # обучение с checkpoint'а
+#     # saver.restore(sess, 'checkpoints/______.ckpt')
+#     counter = 0
+#     for e in range(epochs):
+#         # Обучаем сеть
+#         new_state = sess.run(model.initial_state)
+#         loss = 0
+#         for x, y in get_batches(encoded, batch_size, num_steps):
+#             counter += 1
+#             start = time.time()
+#             feed = {model.inputs: x,
+#                     model.targets: y,
+#                     model.keep_prob: keep_prob,
+#                     model.initial_state: new_state}
+#             batch_loss, new_state, _ = sess.run(
+#                 [model.loss,
+#                  model.final_state,
+#                  model.optimizer],
+#                 feed_dict=feed)
+#             
+#             end = time.time()
+#             print('Epoch: {}/{}... '.format(e+1, epochs),
+#                   'Training Step: {}... '.format(counter),
+#                   'Training loss: {:.4f}... '.format(batch_loss),
+#                   '{:.4f} sec/batch'.format((end-start)))
+#             
+#             if (counter % save_every_n == 0):
+#                 saver.save(sess, "checkpoints/i{}_l{}.ckpt".format(counter, lstm_size))
+#                 
+#     saver.save(sess, "checkpoints/i{}_l{}.ckpt".format(counter, lstm_size))
 
 
 # def pick_top_n(preds, vocab_size, top_n=5):
